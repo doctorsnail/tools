d40473a3bf98507317d4a337e97cc0806c642e5a
doctorsnail <cxopensource@163.com>
2017-11-12 09:58:02 +0800

    update zigzag

--- zigzag/main.c
+++ zigzag/main.c
@@ -1,26 +1,26 @@
-#include <stdio.h>
 #include <stdlib.h>
-
+#include <stdio.h>
+#include <string.h>
 #define BUF_SIZE 10485760
 
 int main()
 {
 	char *buf = malloc(BUF_SIZE);
 	char lines[64] = "";
-	long linenumber, sl;
+	long ln, sl;
 	long jump_max, i, j, m;
-	gets(buf);
-	sl = strlen(buf);
-	printf("%ld:%s\n", sl, buf);	
-	gets(lines);
-	linenumber = atol(lines);
-	printf("number:%ld\n", linenumber);	
+	fgets(buf, BUF_SIZE, stdin);
+	sl = strlen(buf) - 1;
+	//printf("%ld:%s\n", sl, buf);	
+	fgets(lines, 64, stdin);
+	ln = atol(lines);
+	//printf("number:%ld\n", ln);	
 	
-	if (linenumber <= 0)
+	if (ln <= 0)
 		goto out;
 	
-	jump_max = 2 * linenumber - 2;
-	printf("jump_max:%ld\n", jump_max);
+	jump_max = 2 * ln - 2;
+	//printf("jump_max:%ld\n", jump_max);
 	
 	if (jump_max == 0)
 	{
@@ -28,9 +28,9 @@ int main()
 		goto out;
 	}
 
-	for (i = 0; i < linenumber; i ++)
+	for (i = 0; i < ln; i ++)
 	{
-		m = 2 * (linenumber - i) - 2;
+		m = 2 * (ln - i) - 2;
 		m = jump_max -m;
 
 		for(j = i; j < sl; j += m)
